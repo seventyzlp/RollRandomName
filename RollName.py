191fdf42785fa63c695e7d7bd3312e85ce650e08
giz_soft <15990305624@163.com>
2022-05-06 15:13:52 +0800

    update

--- RollName.py
+++ RollName.py
@@ -1,3 +1,5 @@
 import random
-names=['zlp','gcy','zsy','xmq','zwq','yyl','hyf','hyt','lxy','sza','yzh','zxy','cly','dlxz']
-print(names[random.randint(0,13)])
+
+names = ['zlp', 'gcy', 'zsy', 'xmq', 'zwq', 'yyl', 'hyf', 'hyt', 'lxy', 'sza', 'yzh', 'zxy', 'cly', 'dlxz']
+print(names[random.randint(0, 13)])
+#  hyf略过，在以及roll到的人移除名单
